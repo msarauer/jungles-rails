fc7d88afc31706003d37c1681829ac9c65c4d9a6
Mitchel Sarauer <mitchelsarauer@hotmail.com>
2021-06-28 17:36:42 -0600

    with this added, first_name and last name are required

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ActiveRecord::Base
 
   has_secure_password
   validates :email, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   validates_uniqueness_of :email, :case_sensitive => false
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,24 +3,45 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe "Validations" do
     it("should require a password") do
-      @user = User.create({email: 'a@test.com'})
+      @user = User.create({first_name: "Mitchel", last_name: "Sara", email: 'a@test.com'})
       puts "password error: ", @user.errors.full_messages
       expect(@user.errors.full_messages).to eq(["Password can't be blank"])
       expect(@user).to_not be_valid
     end
+
     it("should create a new user if passwords match") do
-      @user = User.create({email: 'a@test.com', password: "asd", password_confirmation: "asd"})
+      @user = User.create({first_name: "Mitchel", last_name: "Sara", email: 'a@test.com', password: "asd", password_confirmation: "asd"})
       
       expect(@user).to be_valid
     end
 
     it("should not create a new user if the passwords do not match") do
-      @user = User.create({email: 'as@test.com', password: "as", password_confirmation: "asd"})
+      @user = User.create({first_name: "Mitchel", last_name: "Sara", email: 'as@test.com', password: "as", password_confirmation: "asd"})
       puts "password error2: ", @user.errors.full_messages
       expect(@user.errors.full_messages).to eq(["Password confirmation doesn't match Password"])
       expect(@user).to_not be_valid
     end
 
+    it("should have case insenstive emails") do
+      @user = User.create({first_name: "Mitchel", last_name: "Sara", email: "m@test.com", password: "asd", password_confirmation: "asd"})
+      @user1 = User.create({first_name: "Mitchel", last_name: "Sara", email: "M@test.com", password: "asd", password_confirmation: "asd"})
+      expect(@user1.errors.full_messages).to eq(["Email has already been taken"])
+      expect(@user1).to_not be_valid
+    end
+
+    it("should require a first name") do
+      @user = User.create({ last_name: "Sara", email: "m@test.com", password: "asd", password_confirmation: "asd"})
+      puts "password error: ", @user.errors.full_messages
+      expect(@user.errors.full_messages).to eq(["First name can't be blank"])
+      expect(@user).to_not be_valid
+    end
+
+    it("should require a last name") do
+      @user = User.create({ first_name: "Sara", email: "m@test.com", password: "asd", password_confirmation: "asd"})
+      puts "password error: ", @user.errors.full_messages
+      expect(@user.errors.full_messages).to eq(["Last name can't be blank"])
+      expect(@user).to_not be_valid
+    end
 
   end
 end

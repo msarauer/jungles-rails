7893c1f2f4737eeb21109f8695455aaa0525574b
Mitchel Sarauer <mitchelsarauer@hotmail.com>
2021-06-28 17:43:22 -0600

    with this added, passwords need to be a min of 3 length. A test is also added for this

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,41 +5,54 @@ RSpec.describe User, type: :model do
     it("should require a password") do
       @user = User.create({first_name: "Mitchel", last_name: "Sara", email: 'a@test.com'})
       puts "password error: ", @user.errors.full_messages
-      expect(@user.errors.full_messages).to eq(["Password can't be blank"])
+      expect(@user.errors.full_messages).to include("Password can't be blank")
       expect(@user).to_not be_valid
     end
 
     it("should create a new user if passwords match") do
-      @user = User.create({first_name: "Mitchel", last_name: "Sara", email: 'a@test.com', password: "asd", password_confirmation: "asd"})
+      @user = User.create({first_name: "Mitchel", last_name: "Sara", email: 'a@test.com', password: "asd12", password_confirmation: "asd12"})
       
       expect(@user).to be_valid
     end
 
     it("should not create a new user if the passwords do not match") do
-      @user = User.create({first_name: "Mitchel", last_name: "Sara", email: 'as@test.com', password: "as", password_confirmation: "asd"})
+      @user = User.create({first_name: "Mitchel", last_name: "Sara", email: 'as@test.com', password: "as12", password_confirmation: "asd12"})
       puts "password error2: ", @user.errors.full_messages
       expect(@user.errors.full_messages).to eq(["Password confirmation doesn't match Password"])
       expect(@user).to_not be_valid
     end
 
     it("should have case insenstive emails") do
-      @user = User.create({first_name: "Mitchel", last_name: "Sara", email: "m@test.com", password: "asd", password_confirmation: "asd"})
-      @user1 = User.create({first_name: "Mitchel", last_name: "Sara", email: "M@test.com", password: "asd", password_confirmation: "asd"})
+      @user = User.create({first_name: "Mitchel", last_name: "Sara", email: "m@test.com", password: "asd12", password_confirmation: "asd12"})
+      @user1 = User.create({first_name: "Mitchel", last_name: "Sara", email: "M@test.com", password: "asd12", password_confirmation: "asd12"})
       expect(@user1.errors.full_messages).to eq(["Email has already been taken"])
       expect(@user1).to_not be_valid
     end
 
     it("should require a first name") do
-      @user = User.create({ last_name: "Sara", email: "m@test.com", password: "asd", password_confirmation: "asd"})
+      @user = User.create({ last_name: "Sara", email: "m@test.com", password: "asd12", password_confirmation: "asd12"})
       puts "password error: ", @user.errors.full_messages
       expect(@user.errors.full_messages).to eq(["First name can't be blank"])
       expect(@user).to_not be_valid
     end
 
     it("should require a last name") do
-      @user = User.create({ first_name: "Sara", email: "m@test.com", password: "asd", password_confirmation: "asd"})
-      puts "password error: ", @user.errors.full_messages
+      @user = User.create({ first_name: "Sara", email: "m@test.com", password: "asd12", password_confirmation: "asd12"})
       expect(@user.errors.full_messages).to eq(["Last name can't be blank"])
+      expect(@user).to_not be_valid
+    end
+
+    it("should require an email") do
+      @user = User.create({ first_name: "Sara", last_name: "asd", password: "asd12", password_confirmation: "asd12"})
+
+      expect(@user.errors.full_messages).to include("Email can't be blank")
+      expect(@user).to_not be_valid
+    end
+
+    it("should require a length of 3 for passwords") do
+      @user = User.create({first_name: "Mitchel", last_name: "Sara", email: 'a@test.com', password: "as", password_confirmation: "as"})
+      
+      expect(@user.errors.full_messages).to include("Password is too short (minimum is 3 characters)")
       expect(@user).to_not be_valid
     end
 
